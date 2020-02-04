cb94d36cc6412ebdf95c9b14bd99872d1a20fd7d
Mads Mikkel Rasmussen <mara@aspit.dk>
2020-02-04 11:19:10 +0100

    Added Test project. Added sql statement, SqlConnection and SqlCommand objects.

--- MadsMikkel.HandBook.DataAccess.SqlClient/EmployeeRepository.cs
+++ MadsMikkel.HandBook.DataAccess.SqlClient/EmployeeRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
 
 namespace MadsMikkel.HandBook.DataAccess.SqlClient
 {
@@ -14,5 +16,17 @@ namespace MadsMikkel.HandBook.DataAccess.SqlClient
             ApplicationIntent=ReadWrite;
             MultiSubnetFailover=False
             ";
+
+        public List<Employee> GetAll()
+        {
+            List<Employee> employees = new List<Employee>();
+            string sql = "SELECT (EmployeeID, FirstName, LastName, Title, " +
+                "TitleOfCourtesy, BirthDate, HireDate) FROM Employees";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            return employees;
+        }
     }
 }
---
+++ TestProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MadsMikkel.HandBook.DataAccess.SqlClient;
+
+namespace TestProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeRepository repository = new EmployeeRepository();
+            repository.GetAll();
+        }
+    }
+}
