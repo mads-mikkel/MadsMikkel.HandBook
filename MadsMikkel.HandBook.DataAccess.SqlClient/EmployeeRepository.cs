5a1fd9a69400ca619b307631054c6777eab3d900
Mads Mikkel Rasmussen <mara@aspit.dk>
2020-02-04 10:50:16 +0100

    Added the connection string.

--- MadsMikkel.HandBook.DataAccess.SqlClient/EmployeeRepository.cs
+++ MadsMikkel.HandBook.DataAccess.SqlClient/EmployeeRepository.cs
@@ -4,6 +4,15 @@ namespace MadsMikkel.HandBook.DataAccess.SqlClient
 {
     public class EmployeeRepository
     {
-        private const string connectionString = @"";
+        private const string connectionString = @"
+            Data Source=(localdb)\MSSQLLocalDB;
+            Initial Catalog=Northwind;
+            Integrated Security=True;
+            Connect Timeout=30;
+            Encrypt=False;
+            TrustServerCertificate=False;
+            ApplicationIntent=ReadWrite;
+            MultiSubnetFailover=False
+            ";
     }
 }
