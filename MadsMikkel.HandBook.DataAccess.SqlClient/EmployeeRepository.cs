6ad50cdf9df9931b836ae28433433b7dd2ca1a96
Mads Mikkel Rasmussen <mara@aspit.dk>
2020-02-04 12:58:38 +0100

    Added GetFieldValue extration.

--- MadsMikkel.HandBook.DataAccess.SqlClient/EmployeeRepository.cs
+++ MadsMikkel.HandBook.DataAccess.SqlClient/EmployeeRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
+
 using System;
 using System.Collections.Generic;
 
@@ -20,12 +21,39 @@ namespace MadsMikkel.HandBook.DataAccess.SqlClient
         public List<Employee> GetAll()
         {
             List<Employee> employees = new List<Employee>();
-            string sql = "SELECT (EmployeeID, FirstName, LastName, Title, " +
-                "TitleOfCourtesy, BirthDate, HireDate) FROM Employees";
+            string sql = "SELECT EmployeeID, FirstName, LastName, Title, " +                       
+                "TitleOfCourtesy, BirthDate, HireDate FROM Employees";
 
             SqlConnection connection = new SqlConnection(connectionString);
             SqlCommand command = new SqlCommand(sql, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
 
+            while(reader.Read())
+            {
+                int id = reader.GetFieldValue<int>(0);
+                string firstname = reader.GetFieldValue<string>(1);
+                string lastname = reader.GetFieldValue<string>(2);
+                string title = reader.GetFieldValue<string>(3);
+                string titleOfCourtesy = reader.GetFieldValue<string>(4);
+                DateTime birthDate = reader.GetFieldValue<DateTime>(5);
+                DateTime hireDate = reader.GetFieldValue<DateTime>(6);
+
+                Employee employee = new Employee
+                {
+                    Id = id,
+                    Firstname = firstname,
+                    Lastname = lastname,
+                    Title = title,
+                    TitleOfCourtesy = titleOfCourtesy,
+                    BirthDate = birthDate,
+                    HireDate = hireDate
+                };
+
+                employees.Add(employee);
+            }
+
+            connection.Close();
             return employees;
         }
     }
