8098f6221ddf6baacf95ea02ed5b19d01d1abe4d
Mads Mikkel Rasmussen <mara@aspit.dk>
2020-02-04 10:54:44 +0100

    Added Employee class.

---
+++ MadsMikkel.HandBook.DataAccess.SqlClient/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MadsMikkel.HandBook.DataAccess.SqlClient
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string TitleOfCourtesy { get; set; }
+        public string Title { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+}
