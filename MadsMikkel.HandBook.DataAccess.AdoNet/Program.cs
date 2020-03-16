b0ea625c6a149ad139d115704d466f2b92547a5d
Mads Mikkel Rasmussen <mara@aspit.dk>
2020-03-16 14:58:18 +0100

    Added AdoNet.

---
+++ MadsMikkel.HandBook.DataAccess.AdoNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MadsMikkel.HandBook.DataAccess.AdoNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
